f9c8cb6f190991f66db7787d36f51582f0c7ba4f
Al0102 <mealex2007@gmail.com>
2023-05-17 23:07:37 -0700

    init commit 2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 
+
 class Main:
     def __init__(self):
         pygame.init()
-        self.display = pygame.display.set_mode((1920, 1080))
-        self.background = pygame.image.load("assets/background.png").convert_alpha()
-        self.background = pygame.transform.scale(self.background, (self.background.get_width(), self))
+        self.display = pygame.display.set_mode((1024, 768))
+
+        self.background = pygame.image.load("assets/background.png").convert_alpha() #load image
+        self.background = pygame.transform.scale(self.background, (self.display.get_width(), self.display.get_height())) #fits to new height
         self.bg_rect = self.background.get_rect()
-        self.rect = pygame.Rect(0,0,10,10)
+
         self.running = True
+            
     def run(self):
         while self.running:
             for event in pygame.event.get():
